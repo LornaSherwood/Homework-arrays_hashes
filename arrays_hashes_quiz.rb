d560e805285af6641bfac5d749ee85e29e269111
Lorna Sherwood <lornasherwood@gmail.com>
2016-11-23 16:24:43 +0000

    Finshed question 2

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -59,7 +59,14 @@ puts my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
 
+my_hash[3] = "Three"
+
 # 5. How would you add `{:four => 4}` to the hash?
+
+# my_hash = {:four => 4}
+my_hash[:four] = 4
+
+puts my_hash
 
 ### C. Given the following data structure:
 
