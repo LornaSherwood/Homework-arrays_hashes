820148803e7348b68a01c037cd806541d3a54b4e
Lorna Sherwood <lornasherwood@gmail.com>
2016-11-23 18:14:25 +0000

    Finsihed homework

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -122,8 +122,28 @@ puts users["Erik"][:favourite_numbers][0]
 
 # 6. Add the number `7` to Erik's favorite numbers
 
+users["Erik"][:favourite_numbers].push(7)
+puts users["Erik"][:favourite_numbers]
+
 # 7. Change Erik's hometown to Edinburgh
+
+users["Erik"][:home_town] = "Edinburgh"
+puts users["Erik"][:home_town]
 
 # 8. Add a pet dog to Erik called "Fluffy"
 
+users["Erik"][:pets]["dog"] = "Fluffy"
+puts users["Erik"][:pets]
+
 # 9. Add yourself to the users hash
+
+users["Lorna"] = {
+    :twitter => "lorna_s",
+    :favourite_numbers => [7, 81],
+    :home_town => "Aberdeen",
+    :pets => {
+      "misty" => :dog,
+      "olly" => :octopus
+    }
+}
+puts users
